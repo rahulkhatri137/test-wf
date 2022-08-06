91f10267b5e0cb35dd099ab8856660ba246ebe0b
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-08-06 07:10:28 +0530

    Rice

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-~/.bin/repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico_mr1-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b posp .repo/local_manifests
+~/.bin/repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b riced .repo/local_manifests
