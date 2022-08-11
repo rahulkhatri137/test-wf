710d2f6065c342f18c5e3e7634bc039507d3d459
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-08-11 07:18:46 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 ~/.bin/repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b riced .repo/local_manifests
+git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b twelve .repo/local_manifests
