893fc1cd77a4623d766356f10c0af26fcdfeee56
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-08-15 23:10:09 +0530

    pbrp

--- init.sh
+++ init.sh
@@ -1,2 +1,1 @@
-~/.bin/repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b twelve .repo/local_manifests
+~/.bin/repo init --no-repo-verify -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-9.0
