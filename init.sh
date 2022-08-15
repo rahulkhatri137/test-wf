9799cb1975d29be062ea764e32e350bba393ae21
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-08-15 23:16:05 +0530

    clone dt

--- init.sh
+++ init.sh
@@ -1,1 +1,2 @@
 ~/.bin/repo init --no-repo-verify -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-9.0
+git clone https://github.com/rahulkhatri137/android_recovery_oppo_CPH1861_pbrp -b main device/oppo/CPH1861
