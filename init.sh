978a7ff1faac5ee66cbe1ae4e4c13c915fbc8f39
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-11-03 14:05:07 +0530

    AEX

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-~/.bin/repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b twelve .repo/local_manifests
+~/.bin/repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest -b 12.1.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b aex .repo/local_manifests
