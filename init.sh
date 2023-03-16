3148bdfa07bc72fec1403b5f11b66b84e0ed0a73
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2023-03-16 11:17:44 +0530

    Cipher

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-~/.bin/repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b riced .repo/local_manifests
+~/.bin/repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest -b twelve-L -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b cipher .repo/local_manifests
