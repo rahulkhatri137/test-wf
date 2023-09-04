5289ba5549a850b7d16bb6dcdc9eadf8d2e18363
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2023-09-04 09:15:02 +0530

    init: vars

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
-~/.bin/repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest -b twelve-L -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b cipher .repo/local_manifests
+repo=rahulkhatri137/local_manifest
+branch=cipher
+manifest=https://github.com/CipherOS/android_manifest
+manifest_branch=twelve-L
+~/.bin/repo init --depth=1 --no-repo-verify -u $manifest -b $manifest_branch -g default,-mips,-darwin,-notdefault
+git clone https://github.com/$repo --depth 1 -b $branch .repo/local_manifests
